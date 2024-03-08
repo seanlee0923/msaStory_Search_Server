3e1bca6563b798c67f26661f567d43fbb87837e9
seanlee0923 <leesinue0923@gmail.com>
2024-03-08 22:18:24 +0900

    Fix: 메서드 이름 변경
    메서드 이름 변경

--- src/main/java/com/msastory/search/controller/SearchController.java
+++ src/main/java/com/msastory/search/controller/SearchController.java
@@ -1,9 +1,7 @@
 package com.msastory.search.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.msastory.search.service.SearchService;
@@ -23,7 +21,8 @@ public class SearchController {
     }
     
     @GetMapping("/informations/{nickname}")
-    public String getCharacterInfoBy(@PathVariable("nickname") String nickname) {
-        return searchService.getCharactorInfoBy(nickname);
+    public String getCharacterBasicInfoBy(@PathVariable("nickname") String nickname) {
+        return searchService.getCharactorBasicDataBy(nickname).toString();
     }
+
 }
